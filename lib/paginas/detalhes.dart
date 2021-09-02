4257fecfaf2e3ec49685c90dbcbfda222790f489
gualterapple <39972000+gualterapple@users.noreply.github.com>
2021-09-02 08:50:35 +0100

    Pagina de detalhes adicionada

--- lib/paginas/detalhes.dart
+++ lib/paginas/detalhes.dart
@@ -1,21 +1,36 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:lojinha/modelos/movel.dart';
+import 'package:lojinha/widgets/appbar_customizada.dart';
 
 class Detalhes extends StatelessWidget  {
 
+  final Movel movel;
+
+  Detalhes({required this.movel});
+    
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Detalhes'),
+
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+            'utilidades/assets/imagens/${movel.foto}'),
+            fit: BoxFit.cover
+            )
+          ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBarCustomizada(titulo: '',ehPaginaCarrinho: false),
+        body: TextButton(
+          onPressed: ()
+          {
+            Navigator.pushNamed(context, '/carrinho');
+          },
+          child: Text('Agora para a página carrinho'),
+          ),
       ),
-      body: TextButton(
-        onPressed: ()
-        {
-          Navigator.pushNamed(context, '/carrinho');
-        },
-        child: Text('Agora para a página carrinho'),
-        ),
     );
   }
 }
--- lib/widgets/element_grid_produtos.dart
+++ lib/widgets/element_grid_produtos.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:lojinha/modelos/movel.dart';
+import 'package:lojinha/paginas/detalhes.dart';
 import 'package:lojinha/widgets/degrade_elemento_grid_produto.dart';
 import 'package:lojinha/widgets/imagem_elemento_grid_produto.dart';
 import 'package:lojinha/widgets/titulo_elemento_grid_produto.dart';
@@ -14,12 +16,35 @@ class ElementoGridProdutos extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-     return Stack(
-       alignment: Alignment.center,
-       children:<Widget> 
-       [ImagemElementoGridProdutos(imagem: movel.foto),
-       DegradeElementoGridProdutos(),
-       TituloElementoGridProduto(titulo: movel.titulo)]
+     return InkWell(
+       onTap: (){
+         Navigator.push(context,
+         MaterialPageRoute(builder: (context) => 
+           Detalhes(movel: movel)
+         ));
+       },
+       child: Container(
+         decoration: BoxDecoration(
+           boxShadow: [
+             BoxShadow(
+               spreadRadius: 2,
+               blurRadius: 8,
+               color: Colors.black12
+             )
+           ]
+         ),
+         margin: EdgeInsets.all(10),
+         child: ClipRRect(
+           borderRadius: BorderRadius.circular(8),
+           child: Stack(
+             alignment: Alignment.center,
+             children:<Widget> 
+             [ImagemElementoGridProdutos(imagem: movel.foto),
+             DegradeElementoGridProdutos(),
+             TituloElementoGridProduto(titulo: movel.titulo)]
+           ),
+         ),
+       ),
      );
   }
 }
